35be8f3f1cdd014414b9724786de21f8ca8408f9
Marcio Souza <marciosouza@Marcios-MacBook-Pro.local>
2019-02-14 22:09:12 +0000

    Criada página de exercícios para busca 1.

--- html/assets/js/exercicios.js
+++ html/assets/js/exercicios.js
@@ -52,6 +52,33 @@ function calcularBigOExercicios() {
     $("#resultado").text(resultadoFinal);
 }
 
+function calcularAlgoritmosBusca1Exercicios() {
+    var resultados = [
+        { res: "2", sel: $("input[name='p1']:checked").val() },
+        { res: "4", sel: $("input[name='p2']:checked").val() },
+        { res: "3", sel: $("input[name='p3']:checked").val() },
+        { res: "2", sel: $("input[name='p4']:checked").val() },
+        { res: "3", sel: $("input[name='p5']:checked").val() },
+        { res: "1", sel: $("input[name='p6']:checked").val() }
+    ];
+    var totalAcertos = 0;
+    var resultadoFinal;
+    for (var i in resultados) {
+        if (resultados[i].res === resultados[i].sel) {
+            totalAcertos++;
+        }
+    }
+    resultadoFinal = Math.round(totalAcertos / resultados.length * 100);
+    if (resultadoFinal >= 70) {
+        $(".passou").show();
+        $(".naoPassou").hide();
+    } else {
+        $(".passou").hide();
+        $(".naoPassou").show();
+    }
+    $("#resultado").text(resultadoFinal);
+}
+
 $("input:radio").click(function() {
     $("."+this.name).removeClass("success");
     $("."+this.name).addClass("primary");
