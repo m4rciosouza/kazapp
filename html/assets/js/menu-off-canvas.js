7aaf213e90247b1de5e83da4337a6a0eccd18a8f
Marcio Souza <marciosouza@Marcios-MacBook-Pro.local>
2019-02-16 11:59:56 +0000

    Removida opção de tradução.

--- html/assets/js/menu-off-canvas.js
+++ html/assets/js/menu-off-canvas.js
@@ -8,14 +8,14 @@ var menuOffCavas = '' +
             '<br />' +
             '<br />' +
             '<ul class="vertical menu" data-accordion-menu>' +
-              '<li>' +
+              '<li style="display: none">' +
                 '<a href="#0"><span class="title-bar-title i18n-off-canvas-1">Língua</span></a>' +
                 '<ul class="menu vertical nested is-active">' +
                   '<li><a href="#0" onclick="I18nTraducao.definirLingua(\'en_GB\');"><img src="/html/assets/img/en_GB.png" style="width: 20px; height: 20px"> <span class="title-bar-title i18n-off-canvas-2">Inglês</span></a></li>' +
                   '<li><a href="#0" onclick="I18nTraducao.definirLingua(\'pt_BR\');"><img src="/html/assets/img/pt_BR.png" style="width: 20px; height: 20px"> <span class="title-bar-title i18n-off-canvas-3">Português</span></a></li>' +
                 '</ul>' +
               '</li>' +
-              '<li><a href="#0"><span class="title-bar-title i18n-off-canvas-4">Sobre :)</span></a></li>' +
+              '<li><a href="#0"><span class="title-bar-title i18n-off-canvas-4">Sobre</span></a></li>' +
             '</ul>' +
         '</div>' +
         '<div class="off-canvas position-right" id="offCanvasRight" data-off-canvas data-position="right">' +
@@ -28,7 +28,7 @@ var menuOffCavas = '' +
                 '<button class="menu-icon" type="button" data-toggle="offCanvasLeft"></button>' +
                 '<span class="title-bar-title i18n-off-canvas-5">Kazapp</span>' +
               '</div>' +
-              '<div class="title-bar-right" style="display: block">' +
+              '<div class="title-bar-right" style="display: none">' +
                 '<button class="menu-icon" type="button" style="display: none"></button>' +
                 '<a href="#" data-toggle="offCanvasLeft" class="menu-flag-pt_BR"><img src="/html/assets/img/pt_BR.png" style="width: 20px; height: 20px">' +
                 '<a href="#" data-toggle="offCanvasLeft" class="menu-flag-en_GB" style="display: none"><img src="/html/assets/img/en_GB.png" style="width: 20px; height: 20px">' +
