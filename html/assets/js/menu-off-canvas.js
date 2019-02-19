4829f86faa408bafb606f33433a48a36285ef45a
Marcio Souza <marciosouza@Marcios-MacBook-Pro.local>
2019-02-19 22:09:38 +0000

    Adicionada primeira versão das pags de inbox e loja.

--- html/assets/js/menu-off-canvas.js
+++ html/assets/js/menu-off-canvas.js
@@ -15,8 +15,10 @@ var menuOffCavas = '' +
                   '<li><a href="#0" onclick="I18nTraducao.definirLingua(\'pt_BR\');"><img src="/html/assets/img/pt_BR.png" style="width: 20px; height: 20px"> <span class="title-bar-title i18n-off-canvas-3">Português</span></a></li>' +
                 '</ul>' +
               '</li>' +
-              '<li><a href="sobre.html"><span class="title-bar-title i18n-off-canvas-4">Sobre</span></a></li>' +
-              '<li><a href="redes-sociais.html"><span class="title-bar-title i18n-off-canvas-4">Redes sociais</span></a></li>' +
+              '<li><a href="sobre.html"><i class="fi-info" style="font-size: 20px; color: black"></i> <span class="title-bar-title i18n-off-canvas-4">Sobre</span></a></li>' +
+              '<li><a href="redes-sociais.html"><i class="fi-address-book" style="font-size: 20px; color: black"></i> <span class="title-bar-title i18n-off-canvas-4">Redes sociais</span></a></li>' +
+              '<li><a href="loja.html"><span class="title-bar-title i18n-off-canvas-4"><i class="fi-shopping-bag" style="font-size: 20px; color: black"></i> Loja</span></a></li>' +
+              '<li><a href="mailbox.html"><span class="title-bar-title i18n-off-canvas-4"><i class="fi-mail" style="font-size: 20px; color: black"></i> Mensagens</span></a></li>' +
             '</ul>' +
         '</div>' +
         '<div class="off-canvas position-right" id="offCanvasRight" data-off-canvas data-position="right">' +
@@ -29,10 +31,13 @@ var menuOffCavas = '' +
                 '<button class="menu-icon" type="button" data-toggle="offCanvasLeft"></button>' +
                 '<span class="title-bar-title i18n-off-canvas-5">Kazapp</span>' +
               '</div>' +
-              '<div class="title-bar-right" style="display: none">' +
+              '<div class="title-bar-right" style="display: block">' +
                 '<button class="menu-icon" type="button" style="display: none"></button>' +
-                '<a href="#" data-toggle="offCanvasLeft" class="menu-flag-pt_BR"><img src="/html/assets/img/pt_BR.png" style="width: 20px; height: 20px">' +
-                '<a href="#" data-toggle="offCanvasLeft" class="menu-flag-en_GB" style="display: none"><img src="/html/assets/img/en_GB.png" style="width: 20px; height: 20px">' +
+                '<a href="#" data-toggle="offCanvasLeft" class="menu-flag-pt_BR" style="display: none"><img src="/html/assets/img/pt_BR.png" style="width: 20px; height: 20px" /></a>' +
+                '<a href="#" data-toggle="offCanvasLeft" class="menu-flag-en_GB" style="display: none"><img src="/html/assets/img/en_GB.png" style="width: 20px; height: 20px" /></a>' +
+                '<a href="loja.html" alt="Loja"><i class="fi-shopping-bag" style="font-size: 20px; color: white"></i><span class="alert badge">5</span></a>' +
+                '&nbsp;&nbsp;&nbsp;' +
+                '<a href="mailbox.html" alt="Mensagens"><i class="fi-mail" style="font-size: 20px; color: white"></i><span class="alert badge">1</span></a>'+
               '</div>' +
             '</div>' +
         '</div>' +
